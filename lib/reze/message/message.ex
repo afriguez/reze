f9a8f12d2964ef7e3d8f830a1241fb6a949509a2
Fer L. <afriguez@gmail.com>
2025-12-01 06:13:38 -0500

    feat(message): add message structs

--- lib/reze/message/message.ex
+++ lib/reze/message/message.ex
@@ -1,3 +1,7 @@
-defmodule Reze.Message do
-  defstruct [:role, :content]
+defmodule Reze.Message.User do
+  defstruct [:content, role: :user]
+end
+
+defmodule Reze.Message.System do
+  defstruct [:content, role: :system, prompt: :reze_default_prompt]
 end
