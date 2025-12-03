b04893514d493f698744f5bcc5765da2e14bbadb
Fer L. <afriguez@gmail.com>
2025-12-03 02:04:09 -0500

    feat(reze): add children to app supervisor

--- lib/reze/application.ex
+++ lib/reze/application.ex
@@ -2,7 +2,10 @@ defmodule Reze.Application do
   use Application
 
   def start(_type, _args) do
-    children = []
+    children = [
+      {Reze.Message.History, max_messages: 7},
+      {Bandit, plug: Reze.Server.Router, scheme: :http}
+    ]
 
     opts = [strategy: :one_for_one, name: Reze.Supervisor]
     Supervisor.start_link(children, opts)
