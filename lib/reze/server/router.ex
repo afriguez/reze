3fb1d4e7113cbdd8e2d0d8ca139761ac12f5d4dd
Fer L. <afriguez@gmail.com>
2025-12-03 02:03:22 -0500

    feat(server): add basic router

---
+++ lib/reze/server/router.ex
@@ -0,0 +1,29 @@
+defmodule Reze.Server.Router do
+  use Plug.Router
+
+  alias Reze.Message.Handler
+  alias Reze.Message
+
+  plug(:match)
+
+  plug(Plug.Parsers,
+    parsers: [:json, :urlencoded],
+    json_decoder: Poison
+  )
+
+  plug(:dispatch)
+
+  post "/messages" do
+    %{"content" => content} = conn.body_params
+
+    {:ok, result} =
+      %Message.User{content: content}
+      |> Handler.handle(opts)
+
+    send_resp(conn, 200, Poison.encode!(result))
+  end
+
+  match _ do
+    send_resp(conn, 404, "Not Found")
+  end
+end
