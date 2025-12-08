969de0673aaa9cfe539cc05244f253a7694ceb2b
Fer L. <afriguez@gmail.com>
2025-12-08 11:51:13 -0500

    feat: update adapter behaviour

--- lib/reze/llm/adapter.ex
+++ lib/reze/llm/adapter.ex
@@ -1,15 +1,13 @@
 defmodule Reze.LLM.Adapter do
+  alias Reze.LLM.Adapters.Deepseek.Stream
   alias Reze.Message, as: Msg
 
-  @callback chat(
-              messages :: list(Msg.User | Msg.System | Msg.Assistant),
-              opts :: Keyword.t()
-            ) ::
+  @callback chat(messages :: list(Msg.User | Msg.System | Msg.Assistant)) ::
               {:ok, response :: Msg.Assistant} | {:error, reason :: term()}
 
-  @callback chat!(
-              messages :: list(Msg.User | Msg.System | Msg.Assistant),
-              opts :: Keyword.t()
-            ) ::
+  @callback chat!(messages :: list(Msg.User | Msg.System | Msg.Assistant)) ::
               response :: Msg.Assistant
+
+  @callback chat_stream(messages :: list(Msg.User | Msg.System | Msg.Assistant)) ::
+              response :: Stream
 end
